ddfa060af5ab1c407517035bb9347a7292182771
Calvin Yu <ycy247@nyu.edu>
2014-06-27 01:34:16 -0400

    Login page, Sign up Page, and Inbox/Message page are done.

---
+++ Ribbit/src/com/yuchihyu/ribbit/RibbitApplication.java
@@ -0,0 +1,16 @@
+package com.yuchihyu.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class RibbitApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "1VXalJo4ftJYmRlygqcJRiwd9MFKawtsQOtjV6AK",
+				"uQl5xgANCy1BkOKavw9sIfIoyGfj1ouxGZwIOAo3");
+		
+	}
+}
