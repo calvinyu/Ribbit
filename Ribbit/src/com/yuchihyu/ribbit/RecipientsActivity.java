d1cb16e3ccb0a6753329030a8ef58d7c9c8709bc
Calvin Yu <ycy247@nyu.edu>
2014-06-30 13:35:25 -0400

    Adding a file to the message part2

--- Ribbit/src/com/yuchihyu/ribbit/RecipientsActivity.java
+++ Ribbit/src/com/yuchihyu/ribbit/RecipientsActivity.java
@@ -18,6 +18,7 @@ import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
@@ -149,6 +150,22 @@ public class RecipientsActivity extends ListActivity {
 		message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getObjectId());
 		message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientsIds());
 		message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
+		
+		byte[] fileBytes =FileHelper.getByteArrayFromFile(this, mMediaUri);
+		
+		if(fileBytes == null) {
+			return null;
+		}
+		else {
+			if(mFileType.equals(ParseConstants.TYPE_IMAGE)) {
+				fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+			}
+			
+			String fileName = FileHelper.getFileName(this, mMediaUri, mFileType);
+			ParseFile file = new ParseFile(fileName, fileBytes);
+			message.put(ParseConstants.KEY_FILE, file);
+		}
+		
 		
 		return message;
 	}
